341ba5a45f15f3bb9d668d13c7948f234c1054fa
贠乾 <yunqian8@live.com>
2019-02-12 17:51:46 +0800

    新增Vue语法测试

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,110 @@
 <template>
   <div id="app">
-    <h3>1,响应式的数据绑定</h3>
-    <input type="text" name="" value="" placeholder="在这里输入文字，下面会跟着变化" v-model="msg">
-		<p>{{ msg }}</p>
+    <h3>1,响应式的数据绑定(文本插值)</h3>
+    <input
+      type="text"
+      name=""
+      value=""
+      placeholder="在这里输入文字，下面会跟着变化"
+      v-model="msg"
+    >
+    <p>{{ msg }}</p>
+    <hr>
+    <h3>2,v-html 指令</h3>
+    <div v-html="message"></div>
+    <hr>
+    <h3>3,v-bind 指令</h3>
+    <label for="r1">修改颜色</label><input
+      type="checkbox"
+      v-model="use"
+      id="r1"
+    >
+    <br><br>
+    <div v-bind:class="{'class1': use}">
+      v-bind:class 指令
+    </div>
+    <hr>
+    <h3>3,复选框</h3>
+    <p>单个复选框：</p>
+    <input
+      type="checkbox"
+      id="checkbox"
+      v-model="checked"
+    >
+    <label for="checkbox">{{ checked }}</label>
+
+    <p>多个复选框：</p>
+    <input
+      type="checkbox"
+      id="runoob"
+      value="Runoob"
+      v-model="checkedNames"
+    >
+    <label for="runoob">Runoob</label>
+    <input
+      type="checkbox"
+      id="google"
+      value="Google"
+      v-model="checkedNames"
+    >
+    <label for="google">Google</label>
+    <input
+      type="checkbox"
+      id="taobao"
+      value="Taobao"
+      v-model="checkedNames"
+    >
+    <label for="taobao">taobao</label>
+    <br>
+    <span>选择的值为: {{ checkedNames }}</span>
+    <hr>
+    <h3>4,单选</h3>
+    <input
+      type="radio"
+      id="runoob"
+      value="Runoob"
+      v-model="picked"
+    >
+    <label for="runoob">Runoob</label>
+    <br>
+    <input
+      type="radio"
+      id="google"
+      value="Google"
+      v-model="picked"
+    >
+    <label for="google">Google</label>
+    <br>
+    <span>选中值为: {{ picked }}</span>
+    <hr>
+    <h3>5,select 列表</h3>
+    <select
+      v-model="selected"
+      name="fruit"
+    >
+      <option value="">选择一个网站</option>
+      <option value="www.runoob.com">Runoob</option>
+      <option value="www.google.com">Google</option>
+    </select>
+
+    <div id="output">
+      选择的网站是: {{selected}}
+    </div>
+    <hr>
+    <h3>5,条件语句</h3>
+    <h4>5-1 v-if 指令</h4>
+    <p v-if="one">我是hello world</p>
+    <template v-if="two">
+      <p>我是模板hello world</p>
+    </template>
+    <h4>5-2 v-else 指令</h4>
+    <div v-if="type === 'A'">
+      A
+    </div>
+    <div v-else>
+      B
+    </div>
+    <h4>5-3 v-else-if 指令  用法和js  C#里面的  else if一样</h4>
     <hr>
   </div>
 </template>
@@ -15,15 +117,23 @@
 // })
 //Vue.component("news",news)
 
-
 export default {
-  name: 'app',
-  data () {
+  name: "app",
+  data() {
     return {
-      msg: 'Hello Welcome to Your Vue.js App'
-    }
+      type: 'B',
+      one: true,
+      two: true,
+      selected: "",
+      picked: "",
+      checked: false,
+      checkedNames: [],
+      use: false,
+      msg: "Hello Welcome to Your Vue.js App",
+      message: '<p style="background:red">Hello</p>'
+    };
   }
-}
+};
 </script>
 
 <style>
@@ -52,5 +162,10 @@ li {
 
 a {
   color: #42b983;
+}
+
+.class1 {
+  background: #444;
+  color: #eee;
 }
 </style>
